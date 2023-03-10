2606ee9297ac8c58d47db0e09196e6b1b0a1548b
davelarde <danielavelarde4@gmail.com>
2023-03-10 15:41:45 -0500

    passed all tests

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
+const { Joke}= require('./db');
+const {Op}= require("sequelize")
+const {Sequelize}= require("sequelize")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +10,15 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const {tags, content}=req.query;
+    let where ={}
+    if(tags){
+      where.tags={[Op.like]: `%${tags}%`} 
+    }
+    if(content){
+      where.joke ={[Op.like]:`%${content}%`}
+    }
+    const jokes = await Joke.findAll({where});
     res.send(jokes);
   } catch (error) {
     console.error(error);
